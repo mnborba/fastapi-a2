03df13214e83caded036dad15080d11df28d6a3a
Marcelo Borba <mnborba@gmail.com>
2025-02-18 13:45:52 -0300

    action para deploy na AWS - Aula 4b

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ descricao = """
 
 
 app = FastAPI(
-    title="FastAPI Aula 4",
+    title="FastAPI Aula 4b",
     description=descricao,
     version="4.3",
     terms_of_service="http://example.com/terms/",
